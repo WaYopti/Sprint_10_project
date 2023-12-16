9a0257e728ee8e54020510ae039e4037d240eae5
WaYopti <153213445+WaYopti@users.noreply.github.com>
2023-12-16 23:52:33 +0300

    10 проект коммит 1

---
+++ configuration.py
@@ -0,0 +1,9 @@
+URL_SERVICE = "https://98119282-f74e-4062-b020-ae037c002d47.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+# POST-запросы
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+CREATE_KIT_PATH = "/api/v1/kits"
---
+++ data.py
@@ -0,0 +1,16 @@
+# В файле будут лежать заголовки и тело запроса.
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer {authToken}"
+}
+
+user_body = {
+    "firstName": "Анатолий",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
+
+kit_body = {
+       "name": "Мой набор"
+}
