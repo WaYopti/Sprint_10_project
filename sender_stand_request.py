15c99f728be5519bd89145878d543bcbe7e88481
WaYopti <153213445+WaYopti@users.noreply.github.com>
2023-12-17 21:25:06 +0300

    коммит 6 (набор создается без авторизации)

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,9 +4,9 @@ import data
 
 
 # функция на создание пользователя
-def post_new_user():
-    url = configuration.URL_SERVICE + configuration.CREATE_USER_PATH  # подставляем полный url до ендпоинта
-    return requests.post(url, json=data.user_body, headers=data.headers)  # передаем тело и заголовки для создания user
+#def post_new_user():
+#    url = configuration.URL_SERVICE + configuration.CREATE_USER_PATH  # подставляем полный url до ендпоинта
+#    return requests.post(url, json=data.user_body, headers=data.headers)  # передаем тело и заголовки для создания user
 
 
 #response = post_new_user()
@@ -14,15 +14,27 @@ def post_new_user():
 #print(response.json())
 
 
-# создание нового набора для созданного ранее пользователя
-def post_new_kit_for_created_user(token):
-    url = configuration.URL_SERVICE + configuration.CREATE_KIT_PATH
+def post_new_auth_kit(name, token):
+    body = data.kit_body.copy()
     body = data.kit_body
     new_headers = data.headers.copy()
-    new_headers["Authorization"] = f"Bearer {token}"
-    return requests.post(url, json=body, headers=new_headers)
+    new_headers["Authorization"] = "Bearer " + token
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_KIT_PATH, json=body, headers=new_headers)
 
-
-response = post_new_kit_for_created_user('token')
+response = post_new_auth_kit('name', 'token')
 print(response.status_code)
 print(response.json())
+
+
+# создание нового набора для созданного ранее пользователя
+#def post_new_kit_for_created_user(token):
+#    url = configuration.URL_SERVICE + configuration.CREATE_KIT_PATH
+#    body = data.kit_body
+#    new_headers = data.headers.copy()
+#    new_headers["Authorization"] = f"Bearer {token}"
+#    return requests.post(url, json=body, headers=new_headers)
+
+
+#response = post_new_kit_for_created_user('token')
+#print(response.status_code)
+#print(response.json())
